6773c10311845f04d015c640add9f0050b883363
SergeyKhval <sergeykhval@gmail.com>
2016-03-02 15:40:40 +0300

    feat: added user service

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -7,61 +7,7 @@
  * Manages authentication to any active providers.
  */
 angular.module('trackWeightApp')
-  .controller('LoginCtrl', function ($scope, Auth, $location, $q, Ref, $timeout) {
-    $scope.passwordLogin = function(email, pass) {
-      $scope.err = null;
-      Auth.$authWithPassword({email: email, password: pass}, {rememberMe: true}).then(
-        redirect, showError
-      );
-    };
-
-    $scope.createAccount = function(email, pass, confirm) {
-      $scope.err = null;
-      if( !pass ) {
-        $scope.err = 'Please enter a password';
-      }
-      else if( pass !== confirm ) {
-        $scope.err = 'Passwords do not match';
-      }
-      else {
-        Auth.$createUser({email: email, password: pass})
-          .then(function () {
-            // authenticate so we have permission to write to Firebase
-            return Auth.$authWithPassword({email: email, password: pass}, {rememberMe: true});
-          })
-          .then(createProfile)
-          .then(redirect, showError);
-      }
-
-      function createProfile(user) {
-        var ref = Ref.child('users/' + user.uid), def = $q.defer();
-        ref.set({email: email, name: firstPartOfEmail(email)}, function(err) {
-          $timeout(function() {
-            if( err ) {
-              def.reject(err);
-            }
-            else {
-              def.resolve(ref);
-            }
-          });
-        });
-        return def.promise;
-      }
-    };
-
-    function firstPartOfEmail(email) {
-      return ucfirst(email.substr(0, email.indexOf('@'))||'');
-    }
-
-    function ucfirst (str) {
-      // inspired by: http://kevin.vanzonneveld.net
-      str += '';
-      var f = str.charAt(0).toUpperCase();
-      return f + str.substr(1);
-    }
-
-
-
+  .controller('LoginCtrl', function ($scope, $location, User) {
     function redirect() {
       $location.path('/account');
     }
@@ -70,5 +16,23 @@ angular.module('trackWeightApp')
       $scope.err = err;
     }
 
+    $scope.passwordLogin = function (user) {
+      $scope.err = null;
 
+      User.passwordLogin(user).then(
+        redirect, showError
+      );
+    };
+
+    $scope.createAccount = function (user) {
+      $scope.err = null;
+      if (!user.pass) {
+        $scope.err = 'Please enter a password';
+      }
+      else if (user.pass !== user.confirm) {
+        $scope.err = 'Passwords do not match';
+      } else {
+        User.createAccount(user).then(redirect, showError);
+      }
+    };
   });
---
+++ app/scripts/services/user.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name trackWeightApp.User
+ * @description
+ * # User
+ * Factory in the trackWeightApp.
+ */
+angular.module('trackWeightApp')
+  .factory('User', function (Auth, $q, Ref, $timeout) {
+
+    function firstPartOfEmail(email) {
+      return ucfirst(email.substr(0, email.indexOf('@')) || '');
+    }
+
+    function ucfirst(str) {
+      // inspired by: http://kevin.vanzonneveld.net
+      str += '';
+      var f = str.charAt(0).toUpperCase();
+      return f + str.substr(1);
+    }
+
+    var User;
+
+    User = {};
+
+    User.createAccount = function (userObj) {
+
+      function createProfile(user) {
+        var ref = Ref.child('users/' + user.uid), def = $q.defer();
+        ref.set({email: userObj.email, name: firstPartOfEmail(userObj.email)}, function (err) {
+          $timeout(function () {
+            if (err) {
+              def.reject(err);
+            }
+            else {
+              def.resolve(ref);
+            }
+          });
+        });
+        return def.promise;
+      }
+
+      return Auth.$createUser({email: userObj.email, password: userObj.pass})
+        .then(function () {
+          // authenticate so we have permission to write to Firebase
+          return Auth.$authWithPassword({email: userObj.email, password: userObj.pass}, {rememberMe: true});
+        })
+        .then(createProfile);
+    };
+
+    User.passwordLogin = function(userObj){
+      return Auth.$authWithPassword({email: userObj.email, password: userObj.pass}, {rememberMe: true});
+    };
+
+    return User;
+  });
